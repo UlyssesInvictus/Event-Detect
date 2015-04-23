d939f344d22a0ac423da4dcc7de19f31e2403196
hsiuchilin <hlin01@college.harvard.edu>
2015-04-22 23:35:40 -0400

    bayes

--- Bayes.py
+++ Bayes.py
@@ -1,4 +1,15 @@
 import Stats.py as s
+from operator import mul
+
+def get_posterior:
 
 
-""" STUFF """
+def bayesian (prior, posterior):
+	event_prob = []
+	non_event_prob = []
+	for (event, non_event) in posterior:
+		event_prob.append(event)
+		non_event_prob.append(non_event)
+	x = reduce(mul, event_prob, 1)
+	y = reduce(mul, non_event_prob, 1)
+	return prior[0]*x > prior[1]*y
