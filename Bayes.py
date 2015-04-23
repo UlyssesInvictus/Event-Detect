98d97110b1f19200ef40bf076f0c20f06bdee4d3
hsiuchilin <hlin01@college.harvard.edu>
2015-04-23 00:20:35 -0400

    Bayesian :)

--- Bayes.py
+++ Bayes.py
@@ -1,9 +1,20 @@
 import Stats.py as s
 from operator import mul
 
-def get_posterior:
+def get_posterior (event_vec, non_event_vec):
+	zipped = zip(event_vec, non_event_vec)
+	post= []
+	for (a, b) in zipped:
+		post.append((a/(a+b), b/(a+b)))
+	return post
 
 
+"""
+input - prior: list, entry one is prior prob of event, entry two is prior prob of non_event
+		posterior: list of tuples, entry one of tuple is penalty for event, entry two
+					is penalty for non_event
+output- bool for event
+"""
 def bayesian (prior, posterior):
 	event_prob = []
 	non_event_prob = []
