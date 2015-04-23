2a50bb0a8aad3cfbe6e28542ba76fe814dad72c3
Raynor Kuang <raynorkuang@gmail.com>
2015-04-22 21:58:58 -0400

    Move utilities_new into utility

--- Utility.py
+++ Utility.py
@@ -1,3 +1,6 @@
+import xlrd
+import string
+
 """
 Input: filename of learning data and number of columns of email data
 Output: Tuple of arrays of arrays of email data. First element in tuple represents event emails. Second element represents non-event emails.
@@ -8,14 +11,34 @@ def read_learning_data(filename, numtypes):
   unevent_data = []
   return (event_data,unevent_data)
 
-
 """
 Input: filename of test data and number of columns of email data
 Output: Array of arrays of email data.
 Assumes certain csv structure TBD. Does not assume particular type of data in each column of csv, with exception of boolean identifying whether event or not. Same as read_learning_data, but doesn’t return tuple.
 """
 def read_test_data(filename, numtypes):
-  test_data = []
+  # open book
+  workbook = xlrd.open_workbook(filename)
+  book = workbook.sheet_by_index(0)
+
+  # initalize keys in the final dictionary
+  num_rows = book.nrows
+  time = ["" for i in xrange(num_rows)]
+  sender = ["" for i in xrange(num_rows)]
+  subject = [[] for i in xrange(num_rows)]
+  message = [[] for i in xrange(num_rows)]
+  remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
+
+  # iterate through excel, reading strings
+  for row in xrange(book.nrows):
+    time[row] = book.cell(row,0).value
+    send_raw = book.cell(row,1).value
+    sender[row] = send_raw[send_raw.find("<")+1:send_raw.find(">")].encode('ascii','ignore') # assumes <"name"> sender format
+    subject[row] = (book.cell(row,2).translate(remove_punctuation_map)).encode('ascii','ignore').split()
+    message[row] = (book.cell(row,3).translate(remove_punctuation_map)).encode('ascii','ignore').split()
+
+  # return dict with each field type
+  test_data = {'time':time, 'sender':sender, 'subject':subject, 'message':message}
   return test_data
 
 
