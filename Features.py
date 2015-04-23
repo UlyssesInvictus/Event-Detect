e8e0c52d2a34fd5ac2d7009669567e16a8d5e205
Raynor Kuang <raynorkuang@gmail.com>
2015-04-23 16:14:08 -0400

    Working set rares, make results prettier

--- Features.py
+++ Features.py
@@ -1,17 +1,35 @@
 import Utility as u
-
+from operator import itemgetter
 
 """
-Input: arrays of email strings for events and non-events, filename (optional), and number of rare words (optional).
-Output: array of relative differences of words. Side-effect of creating a file with rare words line by line. 
-Defaults to filename and number if args not passed.
-Assumes only one type of category.
-Works by concatenating all calls to split_words for both event and non-events, then comparing relative frequencies of each. Sets as rare words the words most distinctive of events.
+Input: (long) arrays of words for events and non-events
+Output: array of tuples of word and relative frequency between event and nonevent, sorted by relative frequency 
+Assumes only one type of category. (Though you can cheat this.)
+Works by calculating frequencies on both arrays, then comparing relative frequencies of each.
 """
-def set_rares(event_words,unevent_words,filename="rares.txt",num_rares=25):
-  relative_frequences = {}
-  sorted_relative = []
-  return sorted_relative
+def get_rares(event_words,nonevent_words):
+  # could take a while, these are long arrays
+  event_freaks = u.get_frequencies(event_words)
+  nonevent_freaks = u.get_frequencies(nonevent_words)
+
+  print event_freaks["your"], len(event_words)
+  print nonevent_freaks["your"], len(nonevent_words)
+
+  all_words = list (set(event_freaks) | set(nonevent_freaks))
+  relative_freaks = [("blank",0) for i in xrange(len(all_words))]
+  word_count = 0
+  for word in all_words:
+    event_count = 0 if word not in event_freaks else event_freaks[word]
+    nonevent_count = 0 if word not in nonevent_freaks else nonevent_freaks[word]
+    relative_freaks[word_count] = (word,event_count/float(len(event_words)) - nonevent_count/float(len(nonevent_words)))
+    word_count+=1
+
+  event_exclusive = list (set(event_freaks) - set(nonevent_freaks))
+  event_exclusive_tuples = [(word,event_freaks[word]) for word in event_exclusive]
+
+  return sorted(relative_freaks,key=lambda tup: tup[1],reverse=True)
+  # return sorted(event_exclusive_tuples,key=lambda tup:tup[1],reverse=True)
+
 
 """
 Input: array of email words and rare words filename (optional).
--- Wrapper.py
+++ Wrapper.py
@@ -12,6 +12,8 @@ print "Enter name of file containing learning set: "
 learning_name = "data/learning_data.xlsx"#raw_input()
 print "Enter name of file containing test set: "
 test_name = "data/test_data.xlsx"#raw_input()
+print "Enter name of rare words file; leave blank to generate in rares.txt"
+rare_name = ""#"human_rares.txt"#raw_input()
 
 learning_data = u.read_learning_data(learning_name,0)
 test_data = u.read_test_data(test_name, 0)
@@ -25,6 +27,27 @@ num_tests = len(test_data[fields[0]])
 """
 FEATURES
 """
+# set rares
+if rare_name == "":
+  rare_name = "rares.txt"
+  # create by frequency
+  all_event_words = []
+  all_nonevent_words = []
+  # concat all words...this could take a while
+  for k in fields:
+    if type(learning_data[0][k][0]) is not list:
+      continue
+    for i in xrange(num_events):
+      all_event_words = all_event_words + learning_data[1][k][i]
+    for i in xrange(num_nonevents):
+      all_nonevent_words = all_nonevent_words + learning_data[0][k][i]
+  rares = f.get_rares(all_event_words,all_nonevent_words)
+  rarefile = open('rares.txt','w')
+  for rare,count in rares[0:51]:
+    rarefile.write(rare+'\n')
+  rarefile.close()
+
+print rares[0:26]
 
 # get int features
 learning_nonevent_features = [[] for i in xrange(num_nonevents)]
@@ -36,23 +59,24 @@ for i in xrange(num_nonevents):
     if type(learning_data[0][k][0]) is not list:
       continue 
     learning_nonevent_features[i] = (learning_nonevent_features[i] + 
-      f.get_features(learning_data[0][k][i]))
+      f.get_features(learning_data[0][k][i], rare_name))
 
 for i in xrange(num_events):
   for k in fields:
     if type(learning_data[0][k][0]) is not list:
       continue 
     learning_event_features[i] = (learning_event_features[i] +
-      f.get_features(learning_data[1][k][i]))
+      f.get_features(learning_data[1][k][i], rare_name))
 
 for i in xrange(num_tests):
   for k in fields:
     if type(learning_data[0][k][0]) is not list:
       continue 
     test_features[i] = (test_features[i] +
-      f.get_features(test_data[k][i]))
+      f.get_features(test_data[k][i], rare_name))
 
 # then convert to bit vectors
+# first, sum all vectors to get average
 sum_nonevents = u.vertical_sum(learning_nonevent_features)
 sum_events = u.vertical_sum(learning_event_features)
 avgs_learning = [float(i)/num_learning for i in u.vertical_sum([sum_events,sum_nonevents])]
@@ -61,6 +85,7 @@ avgs_learning = [float(i)/num_learning for i in u.vertical_sum([sum_events,sum_n
 # print sum_events
 # print avgs_learning
 
+# then supply averages to bit calculation
 learning_nonevent_bits = [f.get_bits(i,avgs_learning) for i in learning_nonevent_features]
 learning_event_bits = [f.get_bits(i,avgs_learning) for i in learning_event_features]
 test_bits = [f.get_bits(i,avgs_learning) for i in test_features]
@@ -73,11 +98,15 @@ test_bits = [f.get_bits(i,avgs_learning) for i in test_features]
 BAYESIAN
 """
 
+# get prior
 prior = (num_events/float(num_learning),num_nonevents/float(num_learning))
+# get posterior
+# first, convert all T/F entries into 1/0 and sum ints
 learning_nonevent_bit_sum = u.vertical_sum([[1 if j else 0 for j in i] for i in learning_nonevent_bits])
 learning_event_bit_sum = u.vertical_sum([[1 if j else 0 for j in i] for i in learning_event_bits])
+# then calculate posterior
 posterior = b.get_posterior(learning_event_bit_sum,learning_nonevent_bit_sum)
-
+# guess!
 guesses = [b.bayesian(prior, i, posterior) for i in test_bits]
 
 """
@@ -85,12 +114,16 @@ RESULTS
 """
 
 # print distro (commented while format is still uncertain)
-print "Is Event | Email Subject | Email Body"
+print "Is Event | First 10 Words of Email Subject | First 10 Words of Email Body"
 for i in xrange(len(guesses)):
-  print guesses[i], " | ", test_data["subject"][i], " | ", test_data["message"][i]
+  subject = ' '.join(test_data["subject"][i][:min(10,len(test_data["subject"][i]))])
+  message = ' '.join(test_data["message"][i][:min(10,len(test_data["message"][i]))])
+  print guesses[i], " | ", subject, " | ", message
 
 # TEST ONLY
 true_ids = [True if i == 1 else False for i in u.read_event_ids(test_name,4)]
-print "guesses: ", guesses
-print "trues: ", true_ids
+# print "guesses: ", guesses
+# print "trues: ", true_ids
 print "accuracy: ", u.get_accuracy(true_ids,guesses)
+# print "prior: ", prior
+# print "posterior: ", posterior
